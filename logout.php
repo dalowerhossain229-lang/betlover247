eea375c23fc915927179ee125dd03fea6f322b56
dalowerhossain229-lang <dalowerhossain229@gmail.com>
2026-04-19 18:09:29 +0600

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,8 +2,6 @@
 session_start();
 session_unset();
 session_destroy();
-// ব্রাউজারের ক্যাশ মেমোরি ক্লিয়ার করার জন্য
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Location: index.php");
+header("Location: index.php"); // লগআউট হওয়ার পর হোম পেজে নিয়ে যাবে
 exit();
 ?>
