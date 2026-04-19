011a65e7f7a37994a541c0fe51ab87a94daf7574
dalowerhossain229-lang <dalowerhossain229@gmail.com>
2026-04-19 19:38:35 +0600

    Update db.php

--- db.php
+++ db.php
@@ -3,12 +3,12 @@ $host = getenv('DB_HOST');
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASS');
 $db   = getenv('DB_NAME');
-$port = getenv('DB_PORT'); // নতুন পোর্ট ভেরিয়েবল
+$port = getenv('DB_PORT');
 
-$conn = new mysqli($host, $user, $pass, $db, $port);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$conn = mysqli_init();
+// SSL এনাবল করা হচ্ছে Aiven এর জন্য
+if (!$conn->real_connect($host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 $conn->set_charset("utf8mb4");
 ?>
