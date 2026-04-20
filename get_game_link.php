fa0e08291a1b1f1af681ca05846e169cd9e47856
dalowerhossain229-lang <dalowerhossain229@gmail.com>
2026-04-21 03:09:24 +0600

    Delete get_game_link.php

--- get_game_link.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'db.php';
-$name = $_GET['name'] ?? '';
-// গেমের নাম অনুযায়ী লিঙ্ক খুঁজে আনা
-$res = $conn->query("SELECT link FROM games WHERE name='$name'");
-$data = $res->fetch_assoc();
-echo json_encode(["link" => $data['link'] ?? '']);
-?>
