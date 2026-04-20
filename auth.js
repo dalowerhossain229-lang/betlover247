38e38e3f3c1f282f38cc8553568668c765219b7d
dalowerhossain229-lang <dalowerhossain229@gmail.com>
2026-04-21 03:19:55 +0600

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,5 @@
+<script>
+    function openLogin() { alert("লগইন ফর্ম ওপেন হচ্ছে..."); /* এখানে ফর্মের কোড আসবে */ }
+    function openRegister() { alert("রেজিস্ট্রেশন ফর্ম ওপেন হচ্ছে..."); }
+    function openPromo() { alert("নতুন অফারগুলো শীঘ্রই আসছে!"); }
+</script>
