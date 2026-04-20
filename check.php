1d235354ec4de35baa83746c59a0ecb84a5d0520
dalowerhossain229-lang <dalowerhossain229@gmail.com>
2026-04-21 03:07:52 +0600

    Delete check.php

--- check.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'db.php';
-$res = $conn->query("SELECT * FROM users");
-while($row = $res->fetch_assoc()) {
-    echo "User: " . $row['username'] . " | Balance: " . $row['balance'] . "<br>";
-}
-?>
