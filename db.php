e919d032fb477b193f1f23d12fc989bfcea1738e
dalowerhossain229-lang <dalowerhossain229@gmail.com>
2026-04-21 03:15:20 +0600

    Create db.php

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$db   = getenv('DB_NAME');
+$port = getenv('DB_PORT') ?: 15764;
+
+$conn = mysqli_init();
+if (!$conn->real_connect($host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Database Connection Failed!");
+}
+$conn->set_charset("utf8mb4");
+?>
