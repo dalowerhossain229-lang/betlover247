87635094deb850da91509662e3437e40a92f1901
dalowerhossain229-lang <dalowerhossain229@gmail.com>
2026-04-22 05:32:17 +0600

    Update login_proc.php

--- login_proc.php
+++ login_proc.php
@@ -11,6 +11,9 @@ if ($row = $res->fetch_assoc()) {
     if (password_verify($pass, $row['password'])) {
         $_SESSION['user_id'] = $row['username'];
         $_SESSION['balance'] = $row['balance'];
+                $user_ip = $_SERVER['REMOTE_ADDR'];
+        $conn->query("UPDATE users SET last_ip = '$user_ip' WHERE username = '$user'");
+
         echo json_encode(["status" => "success"]);
     } else {
         echo json_encode(["status" => "error", "message" => "ভুল পাসওয়ার্ড!"]);
