8ce138e65a5245ec3f25c1ef1a31fcda96aa91d6
dalowerhossain229-lang <dalowerhossain229@gmail.com>
2026-04-23 11:41:11 +0600

    Update update_password_proc.php

--- update_password_proc.php
+++ update_password_proc.php
@@ -3,20 +3,28 @@ session_start();
 include 'db.php';
 header('Content-Type: application/json');
 
-$user = $_SESSION['user_id'];
-$old = $_POST['old_pass'];
-$new = $_POST['new_pass'];
+// ১. লগইন চেক
+if (!isset($_SESSION['user_id'])) {
+    echo json_encode(["status" => "error", "message" => "দয়া করে আবার লগইন করুন।"]);
+    exit;
+}
 
-// পুরাতন পাসওয়ার্ড চেক
+$user = $_SESSION['user_id'];
+// trim ব্যবহার করা হয়েছে যাতে বাড়তি স্পেস এরর না দেয়
+$old = trim($_POST['old_pass']); 
+$new = trim($_POST['new_pass']);
+
+// ২. পুরাতন পাসওয়ার্ড চেক
 $res = $conn->query("SELECT password FROM users WHERE username = '$user'");
 $u_data = $res->fetch_assoc();
 
-if ($old !== $u_data['password']) {
+// ৩. পাসওয়ার্ড যাচাই (সব ভার্সনের জন্য নিরাপদ)
+if (!$u_data || $old !== $u_data['password']) {
     echo json_encode(["status" => "error", "message" => "পুরাতন পাসওয়ার্ড ভুল!"]);
     exit;
 }
 
-// নতুন পাসওয়ার্ড আপডেট
+// ৪. নতুন পাসওয়ার্ড আপডেট
 if ($conn->query("UPDATE users SET password = '$new' WHERE username = '$user'")) {
     echo json_encode(["status" => "success", "message" => "পাসওয়ার্ড সফলভাবে পরিবর্তন হয়েছে!"]);
 } else {
