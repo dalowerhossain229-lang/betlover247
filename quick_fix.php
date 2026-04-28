6db3112e8ac5e595a536be28f63f84099331b589
dalowerhossain229-lang <dalowerhossain229@gmail.com>
2026-04-29 03:59:43 +0600

    Delete quick_fix.php

--- quick_fix.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'db.php';
-// এখানে আপনি যা সেট করবেন, প্রোফাইলে হুবহু তাই দেখাবে
-$conn->query("UPDATE settings SET bonus_target = 5000, pb_target = 10000 WHERE id = 1");
-echo "<h1>✅ Database Targets Updated!</h1>";
-?>
