f8414c5ce63bc88506bec2e9ae5448ef3f666280
dalowerhossain229-lang <dalowerhossain229@gmail.com>
2026-04-29 09:08:43 +0600

    Delete zero_target_fix.php

--- zero_target_fix.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'db.php';
-// ডাটাবেসের সব টার্গেট একদম জিরো করে দেওয়া
-$conn->query("UPDATE settings SET bonus_target = 0, pb_target = 0 WHERE id = 1");
-echo "✅ এখন আপনার সব টার্গেট ০ হয়ে গেছে! প্রোফাইল চেক করুন।";
-?>
